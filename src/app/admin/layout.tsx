03fb86c01f730757e1a286c7c995c5ab3594c05d
Shah Mubaruk <shahmubaruk1999@gmail.com>
2025-11-20 18:30:33 +0000

    <div className="flex">   <AdminSidebar />
      <main className="flex-1 ove

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,9 +7,10 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex">
       <AdminSidebar />
-      <div className="flex-1 p-6 bg-gray-50 min-h-screen">
+
+      <main className="flex-1 overflow-y-auto p-6">
         {children}
-      </div>
+      </main>
     </div>
   );
 }
